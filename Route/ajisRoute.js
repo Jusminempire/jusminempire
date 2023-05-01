d3f59da2e46e563b7599a42f3f100fcd64a021d3
Justice Isreal <justiceyba@gmail.com>
2023-05-01 16:21:53 +0100

    initial comit

--- Route/ajisRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { vistorsMsg, getVistorsMsg } = require("../Controller/Ajis/Ajismessage");
-
-router.post("/vistorsmsg", vistorsMsg);
-router.get("/getvistorsmsg", getVistorsMsg);
-
-module.exports = router;
