9bdc9c0beac20c5c629d4f61ca5418d2df512f93
Justice Isreal <justiceyba@gmail.com>
2023-05-11 22:39:09 +0100

    static file

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ limit: "500000000mb", extended: true }));
 // middleware
 // app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors());
 app.use(helmet());
 app.set("trust proxy", 1);
 app.use(express.static("public"));
