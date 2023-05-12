f8db7623e5f35ce7186b87d42671fb9ec92960ad
Justice Isreal <justiceyba@gmail.com>
2023-05-12 22:23:24 +0100

    allow cors

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ app.use(cors());
 app.use(helmet());
 app.set("trust proxy", 1);
 app.use(express.static("public"));
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  next();
+});
 // app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 100 }));
 
 // routes
